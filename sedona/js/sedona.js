26b0ced2e9478272161490fac0700b21b924c9a8
krasilovamaria <43666809+krasilovamaria@users.noreply.github.com>
2020-10-27 15:30:23 +0700

    Add files via upload

--- sedona/js/sedona.js
+++ sedona/js/sedona.js
@@ -1,1 +1,11 @@
+var link = document.querySelector(".search-btn");
+var searchform = document.querySelector(".search");
 
+ document.addEventListener("DOMContentLoaded", function() {
+    searchform.classList.remove("js-form");
+  });
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchform.classList.toggle("search-open");
+});
