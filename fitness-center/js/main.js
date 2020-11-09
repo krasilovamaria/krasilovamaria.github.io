1a2c5ef4500268ea645f4ca1a597676cd63f2360
krasilovamaria <43666809+krasilovamaria@users.noreply.github.com>
2020-11-09 12:18:23 +0700

    Add files via upload

--- fitness-center/js/main.js
+++ fitness-center/js/main.js
@@ -35,3 +35,28 @@ $(document).ready(function () {
   });
 });
 
+var linkNav = document.querySelectorAll('[href^="#"]');
+var V = 0.5;
+for (var i = 0; i < linkNav.length; i++) {
+  linkNav[i].addEventListener('click', function (e) {
+    e.preventDefault();
+    var w = window.pageYOffset;
+    var hash = e.currentTarget.href.replace(/[^#]*(.*)/, '$1');
+    var t = document.querySelector(hash).getBoundingClientRect().top;
+    var start = null;
+    requestAnimationFrame(step);
+    function step(time) {
+      if (start === null) {
+        start = time;
+      }
+      var progress = time - start;
+      var r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+      window.scrollTo(0, r);
+      if (r !== w + t) {
+        requestAnimationFrame(step);
+      } else {
+        location.hash = hash;
+      }
+    }
+  }, false);
+}
