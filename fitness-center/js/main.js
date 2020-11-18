fbbbb27a98f326764f7b2c8d1e97d62525d3ff50
krasilovamaria <43666809+krasilovamaria@users.noreply.github.com>
2020-11-18 18:27:20 +0700

    Add files via upload

--- fitness-center/js/main.js
+++ fitness-center/js/main.js
@@ -1,48 +1,55 @@
 'use strict';
 
+var sliderTrainers = document.querySelector('.slider');
+var sliderComments = document.querySelector('.comments__slider');
+
 $(document).ready(function () {
-  $('.multiple-items').slick({
-    arrows: true,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    speed: 1000,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
+  if (sliderTrainers.classList.contains('multiple-items')) {
+    $('.multiple-items').slick({
+      arrows: true,
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      speed: 1000,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 2
+          }
         }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 2
-        }
-      }
-    ]
-  });
-  $('.comments__slider').slick({
-    arrows: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1000
-  });
+      ]
+    });
+  }
+  if (sliderComments.classList.contains('multiple-comments')) {
+    $('.multiple-comments').slick({
+      arrows: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 1000
+    });
+  }
 });
 
 var linkNav = document.querySelectorAll('[href^="#"]');
-var V = 0.5;
+var rate = 0.5;
 for (var i = 0; i < linkNav.length; i++) {
   linkNav[i].addEventListener('click', function (e) {
     e.preventDefault();
-    var w = window.pageYOffset;
+    var heightScroll = window.pageYOffset;
     var hash = e.currentTarget.href.replace(/[^#]*(.*)/, '$1');
-    var t = document.querySelector(hash).getBoundingClientRect().top;
+    var position = document.querySelector(hash).getBoundingClientRect().top;
     var start = null;
     requestAnimationFrame(step);
     function step(time) {
@@ -50,13 +57,30 @@ for (var i = 0; i < linkNav.length; i++) {
         start = time;
       }
       var progress = time - start;
-      var r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
-      window.scrollTo(0, r);
-      if (r !== w + t) {
+      var resultPosition = (position < 0 ? Math.max(heightScroll - progress / rate, heightScroll + position) : Math.min(heightScroll + progress / rate, heightScroll + position));
+      window.scrollTo(0, resultPosition);
+      if (resultPosition !== heightScroll + position) {
         requestAnimationFrame(step);
       } else {
         location.hash = hash;
       }
     }
   }, false);
+}
+
+$('input[type=tel]').mask('+ 7 (999) 999-9999');
+
+var button = document.querySelectorAll('.cards__button');
+var card = document.querySelectorAll('.cards__card');
+var link = document.querySelectorAll('.cards__link');
+
+for (var k = 0; k < button.length; k++) {
+  button[k].addEventListener('click', function () {
+    for (var l = 0; l < button.length; l++) {
+      card[l].classList.remove('cards__active');
+      link[l].classList.remove('cards__link-active');
+    }
+    card[k].classList.toggle('cards__active');
+    link[k].classList.toggle('cards__link-active');
+  });
 }
